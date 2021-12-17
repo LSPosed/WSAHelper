f903b305b708552e0db81eb1b419764b97bcbacd
LoveSy <shana@zju.edu.cn>
2021-12-18 03:43:20 +0800

    Refine codes

--- app/src/main/java/org/lsposed/wsa/helper/AppListItem.kt
+++ app/src/main/java/org/lsposed/wsa/helper/AppListItem.kt
@@ -1,174 +1,22 @@
 package org.lsposed.wsa.helper
 
-import androidx.compose.animation.core.*
 import androidx.compose.foundation.Image
-import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.selection.selectable
 import androidx.compose.foundation.shape.CornerSize
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.Card
-import androidx.compose.material.LocalTextStyle
 import androidx.compose.material.MaterialTheme.typography
-import androidx.compose.material.Text
-import androidx.compose.runtime.*
+import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
-import androidx.compose.ui.draw.clipToBounds
-import androidx.compose.ui.graphics.Brush
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
-import androidx.compose.ui.layout.Placeable
-import androidx.compose.ui.layout.SubcomposeLayout
-import androidx.compose.ui.text.*
-import androidx.compose.ui.text.font.*
-import androidx.compose.ui.text.style.*
-import androidx.compose.ui.unit.TextUnit
+import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.unit.dp
 import com.google.accompanist.drawablepainter.rememberDrawablePainter
-import kotlinx.coroutines.delay
 import org.lsposed.wsa.helper.data.model.App
-
-@Composable
-fun MarqueeText(
-    text: String,
-    modifier: Modifier = Modifier,
-    gradientEdgeColor: Color = Color.Transparent,
-    color: Color = Color.Unspecified,
-    fontSize: TextUnit = TextUnit.Unspecified,
-    fontStyle: FontStyle? = null,
-    fontWeight: FontWeight? = null,
-    fontFamily: FontFamily? = null,
-    letterSpacing: TextUnit = TextUnit.Unspecified,
-    textDecoration: TextDecoration? = null,
-    textAlign: TextAlign? = null,
-    lineHeight: TextUnit = TextUnit.Unspecified,
-    overflow: TextOverflow = TextOverflow.Clip,
-    softWrap: Boolean = true,
-    onTextLayout: (TextLayoutResult) -> Unit = {},
-    style: TextStyle = LocalTextStyle.current,
-) {
-    val createText = @Composable { localModifier: Modifier ->
-        Text(
-            text,
-            textAlign = textAlign,
-            modifier = localModifier,
-            color = color,
-            fontSize = fontSize,
-            fontStyle = fontStyle,
-            fontWeight = fontWeight,
-            fontFamily = fontFamily,
-            letterSpacing = letterSpacing,
-            textDecoration = textDecoration,
-            lineHeight = lineHeight,
-            overflow = overflow,
-            softWrap = softWrap,
-            maxLines = 1,
-            onTextLayout = onTextLayout,
-            style = style,
-        )
-    }
-    var offset by remember { mutableStateOf(0) }
-    val textLayoutInfoState = remember { mutableStateOf<TextLayoutInfo?>(null) }
-    LaunchedEffect(textLayoutInfoState.value) {
-        val textLayoutInfo = textLayoutInfoState.value ?: return@LaunchedEffect
-        if (textLayoutInfo.textWidth <= textLayoutInfo.containerWidth) return@LaunchedEffect
-        val duration = 7500 * textLayoutInfo.textWidth / textLayoutInfo.containerWidth
-        val delay = 1000L
-
-        do {
-            val animation = TargetBasedAnimation(
-                animationSpec = infiniteRepeatable(
-                    animation = tween(
-                        durationMillis = duration,
-                        delayMillis = 1000,
-                        easing = LinearEasing,
-                    ),
-                    repeatMode = RepeatMode.Restart
-                ),
-                typeConverter = Int.VectorConverter,
-                initialValue = 0,
-                targetValue = -textLayoutInfo.textWidth
-            )
-            val startTime = withFrameNanos { it }
-            do {
-                val playTime = withFrameNanos { it } - startTime
-                offset = (animation.getValueFromNanos(playTime))
-            } while (!animation.isFinishedFromNanos(playTime))
-            delay(delay)
-        } while (true)
-    }
-
-    SubcomposeLayout(
-        modifier = modifier.clipToBounds()
-    ) { constraints ->
-        val infiniteWidthConstraints = constraints.copy(maxWidth = Int.MAX_VALUE)
-        var mainText = subcompose(MarqueeLayers.MainText) {
-            createText(Modifier)
-        }.first().measure(infiniteWidthConstraints)
-
-        var gradient: Placeable? = null
-
-        var secondPlaceableWithOffset: Pair<Placeable, Int>? = null
-        if (mainText.width <= constraints.maxWidth) {
-            mainText = subcompose(MarqueeLayers.SecondaryText) {
-                createText(Modifier.fillMaxWidth())
-            }.first().measure(constraints)
-            textLayoutInfoState.value = null
-        } else {
-            val spacing = constraints.maxWidth * 2 / 3
-            textLayoutInfoState.value = TextLayoutInfo(
-                textWidth = mainText.width + spacing,
-                containerWidth = constraints.maxWidth
-            )
-            val secondTextOffset = mainText.width + offset + spacing
-            val secondTextSpace = constraints.maxWidth - secondTextOffset
-            if (secondTextSpace > 0) {
-                secondPlaceableWithOffset = subcompose(MarqueeLayers.SecondaryText) {
-                    createText(Modifier)
-                }.first().measure(infiniteWidthConstraints) to secondTextOffset
-            }
-            gradient = subcompose(MarqueeLayers.EdgesGradient) {
-                Row {
-                    GradientEdge(gradientEdgeColor, Color.Transparent)
-                    Spacer(Modifier.weight(1f))
-                    GradientEdge(Color.Transparent, gradientEdgeColor)
-                }
-            }.first().measure(constraints.copy(maxHeight = mainText.height))
-        }
-
-        layout(
-            width = constraints.maxWidth,
-            height = mainText.height
-        ) {
-            mainText.place(offset, 0)
-            secondPlaceableWithOffset?.let {
-                it.first.place(it.second, 0)
-            }
-            gradient?.place(0, 0)
-        }
-    }
-}
-
-@Composable
-private fun GradientEdge(
-    startColor: Color, endColor: Color,
-) {
-    Box(
-        modifier = Modifier
-            .width(10.dp)
-            .fillMaxHeight()
-            .background(
-                brush = Brush.horizontalGradient(
-                    0f to startColor, 1f to endColor,
-                )
-            )
-    )
-}
-
-private enum class MarqueeLayers { MainText, SecondaryText, EdgesGradient }
-private data class TextLayoutInfo(val textWidth: Int, val containerWidth: Int)
+import org.lsposed.wsa.helper.ui.MarqueeText
 
 @Composable
 fun AppListItem(app: App, onClick: () -> Unit) {
